7f3c276e50355c61cc6fe4614d8837b0f53fef1c
Carol <carolandrades11@gmail.com>
2021-08-22 09:18:57 -0300

    style: colocando background no menu, adicionando efeito hover na tag a

--- src/components/common/menu/index.js
+++ src/components/common/menu/index.js
@@ -3,27 +3,19 @@ import PropTypes from 'prop-types';
 import MenuToggle from './menuToggle/menuToggle';
 import Navigation from './navigation/navigation';
 import Logo from './logo';
-import Box from '../../foundation/box';
+import MenuWrapper from './style';
 
 const Menu = ({ theme, setTheme }) => {
   const [open, setOpen] = useState(false);
 
   return (
-    <Box
-      display="flex"
-      justifyContent="space-between"
-      alignItems="center"
-      height="8vh"
-      zIndex="15"
-      position="sticky"
-      top="0px"
-    >
+    <MenuWrapper>
       <Logo />
       <>
         <MenuToggle open={open} setOpen={setOpen} />
         <Navigation open={open} setOpen={setOpen} theme={theme} setTheme={setTheme} />
       </>
-    </Box>
+    </MenuWrapper>
   );
 };
 
