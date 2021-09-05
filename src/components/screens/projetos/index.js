6f7dde8bc41bf2ddebc30b56cd496af14abef563
Carol <carolandrades11@gmail.com>
2021-09-05 19:34:52 -0300

    refactor: retirando projeto principal

--- src/components/screens/projetos/index.js
+++ src/components/screens/projetos/index.js
@@ -1,28 +1,13 @@
 /* eslint-disable react/prop-types */
 import React from 'react';
-import Link from '../../common/link/link';
 import Box from '../../foundation/box';
 import Text from '../../foundation/text';
-import ListaProjetos, { ProjetoPrincipal, Seta } from './style';
+import ArrowCircle from './animation/arrowCircle';
+import ListaProjetos from './style';
 
-const repositoriosId = [390006089, 380740837, 385367528, 396137846, 370833957];
-// const imagensItem = [
-//   {
-//     src: '/images/LPinstalura.webp',
-//   },
-//   {
-//     src: '/images/LPalurakut.webp',
-//   },
-//   {
-//     src: '/images/LPjavascriptprojetos.webp',
-//   },
-//   {
-//     src: '/images/LPlayouts.webp',
-//   },
-// ];
+const repositoriosId = [397245461, 390006089, 380740837, 385367528, 396137846, 370833957];
 
 // eslint-disable-next-line no-unused-vars
-
 function Projetos({ repositorios }) {
   const repos = repositorios.filter((repo) => repositoriosId.includes(repo.id));
   return (
@@ -31,14 +16,21 @@ function Projetos({ repositorios }) {
       flexDirection="column"
       justifyContent={{
         xs: 'justify',
-        md: 'space-between',
+        md: 'space-around',
       }}
+      minHeight="75vh"
       padding={{
         xs: '0px',
         md: '40px',
       }}
-      paddingLeft="20px"
-      paddingRight="20px"
+      paddingLeft={{
+        xs: '20px',
+        md: '40px',
+      }}
+      paddingRight={{
+        xs: '20px',
+        md: '40px',
+      }}
     >
       <Box
         display="flex"
@@ -62,10 +54,9 @@ function Projetos({ repositorios }) {
       >
         <Box
           display="flex"
-          flexDirection="column"
           justifyContent={{
-            xs: 'justify',
-            md: 'space-between',
+            xs: 'center',
+            md: 'space-around',
           }}
           height="100%"
           width="100%"
@@ -74,52 +65,16 @@ function Projetos({ repositorios }) {
             md: 'left',
           }}
         >
-          <Text tag="h1" variant="subTitleXS" color="fonts.main" marginBottom="50px">
+          <Text tag="h1" variant="titleXS" color="fonts.main" marginBottom="50px" textAlign="left">
             Projetos
             <br />
             Selecionados
           </Text>
-          <Seta href="#lista">
-            <svg width="75" height="70" viewBox="0 0 75 70" fill="none" xmlns="http://www.w3.org/2000/svg">
-              <path d="M21.875 49.5834L53.125 20.4167" stroke="black" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" />
-              <path d="M21.875 20.4167H53.125V49.5834" stroke="black" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" />
-            </svg>
-          </Seta>
+          <ArrowCircle />
         </Box>
-        <ProjetoPrincipal>
-          <Link href="/casePortfolio" title="Case Portfolio">
-            <img src="https://i.postimg.cc/PfSBBWzG/landingpage.png" alt="Imagem da Home do Portfolio" />
-            <Box
-              display="flex"
-              flexDirection="row"
-              justifyContent={{
-                xs: 'justify',
-                md: 'space-between',
-              }}
-              alignItems="center"
-            >
-              <Box>
-                <Text
-                  tag="h2"
-                  variant="paragraph1XS"
-                  color="fonts.main"
-                >
-                  Portfolio
-                </Text>
-                <Text tag="p" variant="paragraph3" color="fonts.light">
-                  Veja como este portfolio foi criado!
-                </Text>
-              </Box>
-              <svg className="arrow" width="55" height="50" viewBox="0 0 75 70" fill="none" xmlns="http://www.w3.org/2000/svg">
-                <path d="M21.875 49.5834L53.125 20.4167" stroke="black" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" />
-                <path d="M21.875 20.4167H53.125V49.5834" stroke="black" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" />
-              </svg>
-            </Box>
-          </Link>
-        </ProjetoPrincipal>
       </Box>
       <Box>
-        <ListaProjetos id="lista">
+        <ListaProjetos id="listaProjetos">
           {repos.map((item) => (
             <li key={item.id}>
               <a
@@ -161,16 +116,16 @@ function Projetos({ repositorios }) {
                         alignItems="center"
                         title="Star"
                       >
-                        <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"><path d="M21.947 9.179a1.001 1.001 0 0 0-.868-.676l-5.701-.453-2.467-5.461a.998.998 0 0 0-1.822-.001L8.622 8.05l-5.701.453a1 1 0 0 0-.619 1.713l4.213 4.107-1.49 6.452a1 1 0 0 0 1.53 1.057L12 18.202l5.445 3.63a1.001 1.001 0 0 0 1.517-1.106l-1.829-6.4 4.536-4.082c.297-.268.406-.686.278-1.065z" /></svg>
-                        <Text tag="p">{item.stargazers_count}</Text>
+                        <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 24 24"><path d="M21.947 9.179a1.001 1.001 0 0 0-.868-.676l-5.701-.453-2.467-5.461a.998.998 0 0 0-1.822-.001L8.622 8.05l-5.701.453a1 1 0 0 0-.619 1.713l4.213 4.107-1.49 6.452a1 1 0 0 0 1.53 1.057L12 18.202l5.445 3.63a1.001 1.001 0 0 0 1.517-1.106l-1.829-6.4 4.536-4.082c.297-.268.406-.686.278-1.065z" /></svg>
+                        <Text tag="p" variant="paragraph3" color="fonts.light">{item.stargazers_count}</Text>
                       </Box>
                       <Box
                         display="flex"
                         alignItems="center"
                         title="Fork"
                       >
-                        <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"><path d="M5.559 8.855c.166 1.183.789 3.207 3.087 4.079C11 13.829 11 14.534 11 15v.163c-1.44.434-2.5 1.757-2.5 3.337 0 1.93 1.57 3.5 3.5 3.5s3.5-1.57 3.5-3.5c0-1.58-1.06-2.903-2.5-3.337V15c0-.466 0-1.171 2.354-2.065 2.298-.872 2.921-2.896 3.087-4.079C19.912 8.441 21 7.102 21 5.5 21 3.57 19.43 2 17.5 2S14 3.57 14 5.5c0 1.552 1.022 2.855 2.424 3.313-.146.735-.565 1.791-1.778 2.252-1.192.452-2.053.953-2.646 1.536-.593-.583-1.453-1.084-2.646-1.536-1.213-.461-1.633-1.517-1.778-2.252C8.978 8.355 10 7.052 10 5.5 10 3.57 8.43 2 6.5 2S3 3.57 3 5.5c0 1.602 1.088 2.941 2.559 3.355zM17.5 4c.827 0 1.5.673 1.5 1.5S18.327 7 17.5 7 16 6.327 16 5.5 16.673 4 17.5 4zm-4 14.5c0 .827-.673 1.5-1.5 1.5s-1.5-.673-1.5-1.5.673-1.5 1.5-1.5 1.5.673 1.5 1.5zM6.5 4C7.327 4 8 4.673 8 5.5S7.327 7 6.5 7 5 6.327 5 5.5 5.673 4 6.5 4z" /></svg>
-                        <Text tag="p">{item.forks}</Text>
+                        <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 24 24"><path d="M5.559 8.855c.166 1.183.789 3.207 3.087 4.079C11 13.829 11 14.534 11 15v.163c-1.44.434-2.5 1.757-2.5 3.337 0 1.93 1.57 3.5 3.5 3.5s3.5-1.57 3.5-3.5c0-1.58-1.06-2.903-2.5-3.337V15c0-.466 0-1.171 2.354-2.065 2.298-.872 2.921-2.896 3.087-4.079C19.912 8.441 21 7.102 21 5.5 21 3.57 19.43 2 17.5 2S14 3.57 14 5.5c0 1.552 1.022 2.855 2.424 3.313-.146.735-.565 1.791-1.778 2.252-1.192.452-2.053.953-2.646 1.536-.593-.583-1.453-1.084-2.646-1.536-1.213-.461-1.633-1.517-1.778-2.252C8.978 8.355 10 7.052 10 5.5 10 3.57 8.43 2 6.5 2S3 3.57 3 5.5c0 1.602 1.088 2.941 2.559 3.355zM17.5 4c.827 0 1.5.673 1.5 1.5S18.327 7 17.5 7 16 6.327 16 5.5 16.673 4 17.5 4zm-4 14.5c0 .827-.673 1.5-1.5 1.5s-1.5-.673-1.5-1.5.673-1.5 1.5-1.5 1.5.673 1.5 1.5zM6.5 4C7.327 4 8 4.673 8 5.5S7.327 7 6.5 7 5 6.327 5 5.5 5.673 4 6.5 4z" /></svg>
+                        <Text tag="p" variant="paragraph3" color="fonts.light">{item.forks}</Text>
                       </Box>
                     </Box>
                   </Box>
--- src/components/screens/projetos/style/index.js
+++ src/components/screens/projetos/style/index.js
@@ -1,87 +1,6 @@
 import styled, { css } from 'styled-components';
 import breakpointsMedia from '../../../../theme/util/breakpoints';
 
-export const ProjetoPrincipal = styled.div`
-    display: flex;
-    justify-content: space-between;
-    max-width: 650px;
-    max-height: 380px;
-    padding-bottom: 20px;
-    margin: 0 auto;
-    transform: none;
-
-    ${breakpointsMedia({
-    md: css`
-        margin-right: 0;
-        transform: translateY(-100px);
-
-        &:hover {
-            opacity: 1;
-        }
-
-        &:hover svg{
-            transform: rotate(0);
-            background-color: ${({ theme }) => theme.colors.details.main.color};
-        }
-    `,
-  })}
-
-    img {
-        display: initial;
-        width: 290px;
-        box-shadow: rgba(0, 0, 0, 0.19) 0px 10px 20px, rgba(0, 0, 0, 0.23) 0px 6px 6px;
-
-    ${breakpointsMedia({
-    md: css`
-        height: 100%;
-        width: 100%;
-    `,
-  })}
-    }
-
-    p {
-        max-width: 300px;
-        line-height: 1.1;
-        letter-spacing: 1px;
-    }
-
-    h2 {
-        padding: 15px 0;
-    }
-
-    svg {
-        display: none;
-    ${breakpointsMedia({
-    md: css`
-        display: initial;
-        border-radius: 50%;
-        transform: rotate(45deg);
-        transition: ease-in-out 0.3s;
-    `,
-  })}
-    }
-`;
-
-export const Seta = styled.a`
-    width: 80px;
-
-    svg {
-        display: none;
-        transform: rotate(134deg);
-        border-radius: 50%;
-
-        &:hover {
-            background-color: ${({ theme }) => theme.colors.details.main.color};
-        }
-
-    ${breakpointsMedia({
-    md: css`
-        display: initial;
-    `,
-  })}
-    }
-`;
-
 const ListaProjetos = styled.ul`
     padding-top: 0;
 
@@ -90,14 +9,13 @@ const ListaProjetos = styled.ul`
         position: relative;
         padding: 20px 0;
         justify-content: center;
-        border-bottom: none;
+        border-bottom: 1px solid ${({ theme }) => theme.colors.fonts.light.color};
 
     ${breakpointsMedia({
     md: css`
         justify-content: space-between;
         align-items: center;
         padding: 20px;
-        border-bottom: 1px solid ${({ theme }) => theme.colors.fonts.light.color};
     `,
   })}
     }
