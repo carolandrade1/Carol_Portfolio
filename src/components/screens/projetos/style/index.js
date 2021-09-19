1d03783721da55b259f408e162c967ecc56d5d23
Carol <carolandrades11@gmail.com>
2021-09-19 12:19:48 -0300

    refactor: pequenos ajustes CSS página Projetos

--- src/components/screens/projetos/style/index.js
+++ src/components/screens/projetos/style/index.js
@@ -83,7 +83,7 @@ export const ProjetoPrincipal = styled.ul`
 
     img {
       width: 100%;
-      /* box-shadow:  15px 15px 25px #767676; */
+      box-shadow: rgba(9, 30, 66, 0.25) 0px 4px 8px -2px, rgba(9, 30, 66, 0.08) 0px 0px 0px 1px;
     }
 
     .arrow {
